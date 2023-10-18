8353abff1f2082bed859727e5d57edaad1835599
therightgift <goziechukwu@gmail.com>
2023-10-18 15:23:26 +0100

    updated bash file

--- CICDScripts/before_install.sh
+++ CICDScripts/before_install.sh
@@ -4,13 +4,13 @@
 #TODO
 
 #creat wcdTest folder if not existing
-# DIR="/var/www/siteBuilderUtility"
-# if [ -d "$DIR" ]; then
-#   echo "${DIR} exists"
-# else
-#   echo "Creating ${DIR} directory"
-#   mkdir ${DIR}
-# fi
+DIR="/var/www/siteBuilderUtility"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
 
 #grant permission to files inside wcdTest folder
-# sudo chmod -R 777 /var/www/siteBuilderUtility
+sudo chmod -R 777 /var/www/siteBuilderUtility
