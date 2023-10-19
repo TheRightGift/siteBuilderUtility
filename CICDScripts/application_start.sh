98f0f2425280b02d9a631c65fde9ba664716b847
therightgift <goziechukwu@gmail.com>
2023-10-19 07:21:39 +0100

    updated CICD

--- CICDScripts/application_start.sh
+++ CICDScripts/application_start.sh
@@ -6,10 +6,11 @@ echo 'App starting'
 sudo chmod -R 777 /var/www/siteBuilderUtility
 
 #Grant permission to files in required folder
+sudo chown -R www-data.www-data /var/www/siteBuilderUtility/public
 sudo chown -R www-data.www-data /var/www/siteBuilderUtility/src/API
 sudo chown -R www-data.www-data /var/www/siteBuilderUtility/src/Controller
 sudo chown -R www-data.www-data /var/www/siteBuilderUtility/src/storeTemplates
 
 cd /var/www/siteBuilderUtility/src
-sudo chmod -R 777 API && sudo chmod -R 777 Controller && sudo chmod -R 777 storeTemplates
+sudo chmod -R 777 /var/www/siteBuilderUtility/public && sudo chmod -R 777 API && sudo chmod -R 777 Controller && sudo chmod -R 777 storeTemplates
 
